fd91cf0f537178eb4e2062997ae1dd8ee19aadc2
joons5967 <dlwnstjd5967@nate.com>
2023-10-30 11:46:02 +0900

    웹기능사 보일러플레이트

---
+++ js/web.js
@@ -0,0 +1,18 @@
+$("#gnb").click(function(){
+//gnb를 클릭하면 실행
+})
+$("#gnb li").eq(0).click(function(){
+//gnb li의 첫번째를 클릭하면
+})
+$("a img").click(function(){
+//링크 걸린 이미지를 클릭하면 실행
+})
+$("#cart button").click(function(){
+//카트라는 아이디버튼을 클릭하면 실행
+})
+$("#gnb li").find("a").click(function(){
+//자식개체를 찾아라
+})
+$("#gnb li").eq(0).find("a").click(function(){
+//#gnb 안의 첫번쨰 li a요소를 클릭할떄 실행해라.
+})
